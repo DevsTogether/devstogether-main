e847435006ff2347b46bb963185eb2d63511c20f
Wayne Rocha <dev.waynerocha@gmail.com>
2022-04-23 23:04:52 -0300

    <refactor> lint and code quality setup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,29 +1,32 @@
-import {randomBytes} from 'crypto'
-import NextAuth from 'next-auth'
-import FacebookProvider from "next-auth/providers/facebook"
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider from "next-auth/providers/github"
-import CredentialsProvider from "next-auth/providers/credentials"
+import { randomBytes } from 'crypto';
+import NextAuth from 'next-auth';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export default NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
+            clientSecret: process.env.GOOGLE_SECRET,
         }),
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
         }),
         CredentialsProvider({
-            name: "Guest",
+            name: 'Guest',
             async authorize() {
-                return { id: randomBytes(20).toString('hex'), username: "coder-" + randomBytes(20).toString('hex')}
-            }
-        })
-    ]
-})
+                return {
+                    id: randomBytes(20).toString('hex'),
+                    username: 'coder-' + randomBytes(20).toString('hex'),
+                };
+            },
+        }),
+    ],
+});
--- pages/protected/index.js
+++ pages/protected/index.js
@@ -1,19 +1,20 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Protected() {
-  const { data: session } = useSession()
-  if (session) {
+    const { data: session } = useSession();
+    if (session) {
+        return (
+            <>
+                Signed in as {session.user.email || session.user.username}{' '}
+                <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        );
+    }
     return (
-      <>
-        Signed in as {session.user.email || session.user.username} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+        <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+        </>
+    );
 }
