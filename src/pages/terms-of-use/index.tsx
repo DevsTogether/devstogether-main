bfd6568633799c55743381ce505d7dd4a66629fb
Wayne Rocha <62760711+WayneRocha@users.noreply.github.com>
2022-07-31 18:28:34 -0300

    built: paginas de termos e privacidade

---
+++ src/pages/terms-of-use/index.tsx
@@ -0,0 +1,198 @@
+import { NextPage } from 'next';
+
+const termsOfUsePage: NextPage = () => {
+    return (
+        <>
+            <div className="container">
+                <h2>
+                    <span>1. Termos</span>
+                </h2>
+                <p>
+                    <span>
+                        Ao acessar ao site{' '}
+                        <a href="https://devstogether.vercel.app/">
+                            Devs Together
+                        </a>
+                        , concorda em cumprir estes termos de serviço, todas as
+                        leis e regulamentos aplicáveis e concorda que é
+                        responsável pelo cumprimento de todas as leis locais
+                        aplicáveis. Se você não concordar com algum desses
+                        termos, está proibido de usar ou acessar este site. Os
+                        materiais contidos neste site são protegidos pelas leis
+                        de direitos autorais e marcas comerciais aplicáveis.
+                    </span>
+                </p>
+                <h2>
+                    <span>2. Uso de Licença</span>
+                </h2>
+                <p>
+                    <span>
+                        É concedida permissão para baixar temporariamente uma
+                        cópia dos materiais (informações ou software) no site
+                        Devs Together , apenas para visualização transitória
+                        pessoal e não comercial. Esta é a concessão de uma
+                        licença, não uma transferência de título e, sob esta
+                        licença, você não pode:&nbsp;
+                    </span>
+                </p>
+                <ol>
+                    <li>
+                        <span>modificar ou copiar os materiais;&nbsp;</span>
+                    </li>
+                    <li>
+                        <span>
+                            usar os materiais para qualquer finalidade comercial
+                            ou para exibição pública (comercial ou não
+                            comercial);&nbsp;
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            tentar descompilar ou fazer engenharia reversa de
+                            qualquer software contido no site Devs
+                            Together;&nbsp;
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            remover quaisquer direitos autorais ou outras
+                            notações de propriedade dos materiais; ou&nbsp;
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            transferir os materiais para outra pessoa ou
+                            &quot;espelhe&quot; os materiais em qualquer outro
+                            servidor.
+                        </span>
+                    </li>
+                </ol>
+                <p>
+                    <span>
+                        Esta licença será automaticamente rescindida se você
+                        violar alguma dessas restrições e poderá ser rescindida
+                        por Devs Together a qualquer momento. Ao encerrar a
+                        visualização desses materiais ou após o término desta
+                        licença, você deve apagar todos os materiais baixados em
+                        sua posse, seja em formato eletrónico ou impresso.
+                    </span>
+                </p>
+                <h2>
+                    <span>3. Isenção de responsabilidade</span>
+                </h2>
+                <ol>
+                    <li>
+                        <span>
+                            Os materiais no site da Devs Together são fornecidos
+                            &quot;como estão&quot;. Devs Together não oferece
+                            garantias, expressas ou implícitas, e, por este
+                            meio, isenta e nega todas as outras garantias,
+                            incluindo, sem limitação, garantias implícitas ou
+                            condições de comercialização, adequação a um fim
+                            específico ou não violação de propriedade
+                            intelectual ou outra violação de direitos.
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            Além disso, o Devs Together não garante ou faz
+                            qualquer representação relativa à precisão, aos
+                            resultados prováveis ou à confiabilidade do uso dos
+                            materiais em seu site ou de outra forma relacionado
+                            a esses materiais ou em sites vinculados a este
+                            site.
+                        </span>
+                    </li>
+                </ol>
+                <h2>
+                    <span>4. Limitações</span>
+                </h2>
+                <p>
+                    <span>
+                        Em nenhum caso o Devs Together ou seus fornecedores
+                        serão responsáveis por quaisquer danos (incluindo, sem
+                        limitação, danos por perda de dados ou lucro ou devido a
+                        interrupção dos negócios) decorrentes do uso ou da
+                        incapacidade de usar os materiais em Devs Together,
+                        mesmo que Devs Together ou um representante autorizado
+                        da Devs Together tenha sido notificado oralmente ou por
+                        escrito da possibilidade de tais danos. Como algumas
+                        jurisdições não permitem limitações em garantias
+                        implícitas, ou limitações de responsabilidade por danos
+                        conseqüentes ou incidentais, essas limitações podem não
+                        se aplicar a você.
+                    </span>
+                </p>
+                <h2>
+                    <span>5. Precisão dos materiais</span>
+                </h2>
+                <p>
+                    <span>
+                        Os materiais exibidos no site da Devs Together podem
+                        incluir erros técnicos, tipográficos ou fotográficos.
+                        Devs Together não garante que qualquer material em seu
+                        site seja preciso, completo ou atual. Devs Together pode
+                        fazer alterações nos materiais contidos em seu site a
+                        qualquer momento, sem aviso prévio. No entanto, Devs
+                        Together não se compromete a atualizar os materiais.
+                    </span>
+                </p>
+                <h2>
+                    <span>6. Links</span>
+                </h2>
+                <p>
+                    <span>
+                        O Devs Together não analisou todos os sites vinculados
+                        ao seu site e não é responsável pelo conteúdo de nenhum
+                        site vinculado. A inclusão de qualquer link não implica
+                        endosso por Devs Together do site. O uso de qualquer
+                        site vinculado é por conta e risco do usuário.
+                    </span>
+                </p>
+                <h3>
+                    <span>Modificações</span>
+                </h3>
+                <p>
+                    <span>
+                        O Devs Together pode revisar estes termos de serviço do
+                        site a qualquer momento, sem aviso prévio. Ao usar este
+                        site, você concorda em ficar vinculado à versão atual
+                        desses termos de serviço.
+                    </span>
+                </p>
+                <h3>
+                    <span>Lei aplicável</span>
+                </h3>
+                <p>
+                    <span>
+                        Estes termos e condições são regidos e interpretados de
+                        acordo com as leis do Devs Together e você se submete
+                        irrevogavelmente à jurisdição exclusiva dos tribunais
+                        naquele estado ou localidade.
+                    </span>
+                </p>
+            </div>
+            <style jsx>{`
+                .container {
+                    padding: 32px;
+                }
+                h2 {
+                    margin: 10px auto;
+                    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
+                        Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+                        'Helvetica Neue', sans-serif;
+                    font-weight: bolder;
+                }
+                p {
+                    font-size: 16px;
+                }
+                a {
+                    opacity: 0.5;
+                    text-decoration: underline;
+                }
+            `}</style>
+        </>
+    );
+};
+
+export default termsOfUsePage;
