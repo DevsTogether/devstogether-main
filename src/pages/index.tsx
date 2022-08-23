e2bed34d2c1634bb83995ddd8c7536fd65acf09a
Marcos Alves <marcosalvesjr007@gmail.com>
2022-08-23 21:03:08 +0000

    buit: styled components & init login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,45 @@
 /* eslint-disable @next/next/no-img-element */
-
 import type { NextPage } from 'next';
+import { Container, Header } from 'src/styles/pages/home';
 
 const page: NextPage = () => {
-    return (<p>Marcão é show</p>);
+    return (
+        <>
+
+            <Header>
+                <div className="menu">
+                    <img src="/menu.png" />
+                </div>
+                <div className="title">
+                    <img src="/logo2.png" />
+                    <h3>Devstogether</h3>
+                </div>
+
+
+                <nav>
+                    <div className="buttons">
+                        <a className="login" href='#'>Log in</a>
+                        <a className="signup" href='#'>Sign up</a>
+                    </div>
+                </nav>
+            </Header>
+
+            <div align="center">
+                <img src="/logo3.png" />
+            </div>
+
+            <Container>
+
+                <title>Devstogether</title>
+
+                <label className="label1"></label>
+                <button className="button1" href='#'>Log In</button>
+                <button className="button2" href='#'>Log In</button>
+            </Container>
+
+        </>
+
+    );
 };
 
 export default page;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+    colors: {
+      background: '#2D3237',
+      text: '#A1B1C2',
+      primary: '#47AE2E'
+    }
+  }
+  
+  export default theme
